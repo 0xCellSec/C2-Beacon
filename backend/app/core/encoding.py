65601f1839d952611ca75b33cdb5ba281c3e94a9
serenity-cell <justin1726hernandez2@gmail.com>
2026-09-17 00:29:06 +0000

    add toy deffie-hellman key creation

--- backend/app/core/encoding.py
+++ backend/app/core/encoding.py
@@ -1,4 +1,13 @@
 import base64
+import secrets
+import math
+
+GENERATOR = secrets.randbits(256)
+PRIME =  1000000007
+
+host_private_number = secrets.randbits(256)
+beacon_private_number = secrets.randbits(256)
+
 
 
 def XOR_bytes (data: bytes, key: bytes) -> bytes: 
@@ -15,6 +24,12 @@ def XOR_bytes (data: bytes, key: bytes) -> bytes:
     # iterates through every data byte into b and i and peroms a XOR operation against the key
     return bytes(b ^ key[i % len(key)] for i, b in enumerate(data))
 
+def generate_public_key(private_int: int) -> int:
+    return pow(GENERATOR, private_int, PRIME)
+
+def generate_shared_key(primary_private_int: int, secondary_private_int: int) -> int:
+    return pow(generate_public_key(secondary_private_int), primary_private_int, PRIME)
+     
 def encode(payload: str, key: str) -> str:
     """ encode payload with the given key.
     
@@ -43,5 +58,3 @@ def decode (encoded: str, key: str) -> str:
     xored = base64.b64decode(encoded)
     raw = XOR_bytes(xored, key.encode("utf-8"))
     return raw.decode("utf-8")
-
-
